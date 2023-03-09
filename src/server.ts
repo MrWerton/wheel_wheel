faf135f0c46fec50e2739ac027b8d29fb2f08711
werton <mr.werton@gmail.com>
2023-03-09 09:58:04 -0300

    ref: refactored

--- src/server.ts
+++ src/server.ts
@@ -19,10 +19,10 @@ class Player {
 
 class Gamer {
     private _phases = [{ name: 'hello', tip: 'é a traduzido para ola' }, { name: 'world', tip: 'é a traduzido para mundo' }];
-    private currentPhrase;
+    private currentWord;
     private board: string[] = [];
     constructor(private playerOne: Player, private playerTwo: Player) {
-        this.currentPhrase = this.getRandomWord();
+        this.currentWord = this.getRandomWord();
         this.board = this._createBoard();
     }
 
@@ -36,20 +36,20 @@ class Gamer {
         this.playerTwo.notify(`you ${this.playerTwo.points.toString()} \n player one ${this.playerOne.points.toString()}\n`)
     }
     private _createBoard(): string[] {
-        const chars = this.currentPhrase.name.trim().split("");
+        const chars = this.currentWord.name.trim().split("");
         const board: string[] = chars.map((_) => '[]');
 
         return board;
     }
 
     public verifyIfHasWin() {
-        const word = this.currentPhrase.name;
+        const word = this.currentWord.name;
         const wordOfBoard = this.board.join("")
         return word === wordOfBoard;
     }
 
-    private _changeBoardForShowLetter(char: string) {
-        const word = this.currentPhrase.name;
+    private _changeBoardForShowChar(char: string) {
+        const word = this.currentWord.name;
 
         for (let i in this.board) {
             if (word[i] === char) {
@@ -65,7 +65,7 @@ class Gamer {
     }
 
     private _verifyIfUserGuessedChar(char: string) {
-        if (this.currentPhrase.name.includes(char)) {
+        if (this.currentWord.name.includes(char)) {
             return true;
         } else {
             return false;
@@ -76,8 +76,8 @@ class Gamer {
         this.playerOne.notify('Jogador 2 encontrado!\n Voce será o jogador 1\n');
         this.playerTwo.notify("Voce é o jogador 2")
         this.showBoard();
-        this.playerOne.notify(`tip: ${this.currentPhrase.tip}`)
-        this.playerTwo.notify(`tip: ${this.currentPhrase.tip}`)
+        this.playerOne.notify(`tip: ${this.currentWord.tip}`)
+        this.playerTwo.notify(`tip: ${this.currentWord.tip}`)
 
         this.playerOne.socket.write("De acordo com a dica, informe uma letra? ")
 
@@ -87,7 +87,7 @@ class Gamer {
         this.playerOne.socket.on('data', (data) => {
             const result = this._verifyIfUserGuessedChar(data.toString())
             if (result) {
-                this._changeBoardForShowLetter(data.toString())
+                this._changeBoardForShowChar(data.toString())
                 this.playerOne.notify("Voce acertou");
                 this.playerOne.points++;
                 this.showPoints()
@@ -112,7 +112,7 @@ class Gamer {
         this.playerTwo.socket.on('data', (data) => {
             const result = this._verifyIfUserGuessedChar(data.toString())
             if (result) {
-                this._changeBoardForShowLetter(data.toString())
+                this._changeBoardForShowChar(data.toString())
                 this.playerTwo.socket.write("Voce acertou\n");
                 this.playerTwo.points++;
                 this.showPoints()
